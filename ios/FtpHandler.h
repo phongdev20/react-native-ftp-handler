6a913b92ba0fe5a1fca263302b255f4be3594326
tranphong <phongdevinfo@gmail.com>
2025-03-27 14:27:17 +0700

    feat: lib in ios

--- ios/FtpHandler.h
+++ ios/FtpHandler.h
@@ -1,7 +1,19 @@
 
-#import "generated/RNFtpHandlerSpec/RNFtpHandlerSpec.h"
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
+#import <React/RCTBridgeModule.h>
+#endif
 
-@interface FtpHandler : NSObject <NativeFtpHandlerSpec>
+
+#if __has_include("RCTEventEmitter.h")
+#import "RCTEventEmitter.h"
+#else
+#import <React/RCTEventEmitter.h>
+#endif
+
+
+@interface FtpHandler : RCTEventEmitter <RCTBridgeModule>
 
 @end
   
